06db1f6b62f570b7c964ddb76b6578c1791c3060
rjbelleza <rjbelleza19@gmail.com>
2025-03-21 20:26:58 +0800

    Initialized sidemenu

---
+++ src/components/Card1.jsx
@@ -0,0 +1,11 @@
+
+const Card1 = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Card1;
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,16 +1,20 @@
 import Header from "../layouts/Header";
 import AdminSidemenu from "../layouts/AdminSidemenu";
 import Breadcrumb from "../components/Breadcrumb";
+import Card1 from "../components/Card1";
 
 const AdminDashboard = () => {
 
     return (
-        <div className="h-screen bg-gray-300 scroll-smooth">
+        <div className="h-screen w-full bg-gray-300 scroll-smooth">
             <Header />
             <div className="flex h-full w-full mt-15">
                 <AdminSidemenu />
                 <div className="h-full w-full ml-63 mt-2 mr-2 rounded-md">
                     <Breadcrumb />
+                    <div className="h-fit w-full bg-white mt-2 rounded-md">
+                        <Card1 />
+                    </div>
                 </div>
             </div>
         </div>
