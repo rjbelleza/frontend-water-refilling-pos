ec40c260d29ce334a1f062cf9e4ce5d7edd3c80f
rjbelleza <rjbelleza19@gmail.com>
2025-03-21 15:33:17 +0800

    Initialized profile section

--- src/layouts/AdminSidemenu.jsx
+++ src/layouts/AdminSidemenu.jsx
@@ -1,11 +1,20 @@
 
 const AdminSidemenu = () => {
+    const user = {name: 'Jack Frost', email: 'jack@gmail.com', role: 'admin'};
 
     return (
-        <div className="h-full w-60 bg-[url('/images/sidemenu.png')] bg-cover bg-center mt-2 rounded-tr-md
-                        shadow-[2px_0px_10px_gray] opacity-70 fixed left-0"
+        <div className="flex h-full w-60 bg-[url('/images/sidemenu.png')] bg-cover bg-center mt-2 rounded-tr-md
+                        shadow-[2px_0px_10px_gray] bg-opacity-30 fixed left-0 justify-center p-5"
         >
-
+            <div className="flex flex-col gap-1 h-fit w-50 rounded-lg">
+                <div className="flex flex-col items-center w-full">
+                    <p>Welcome,</p>
+                    <p className="font-bold text-[20px]">{user.name}</p>
+                    <p className="w-full text-center text-black font-light text-[20px] mt-3 bg-gray-300 rounded-xl p-1">
+                        {user.role.toLocaleUpperCase()}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
