44736f88fa44c54b00239e4aeb5e897a5903ac4e
rjbelleza <rjbelleza19@gmail.com>
2025-03-21 17:39:44 +0800

    Initialized sidemenu buttons

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -10,8 +10,9 @@ const Breadcrumb = () => {
     }
 
     return (
-        <div className="flex items-center h-13 w-full bg-primary text-white rounded-md sticky top-17">
-            <p className="ml-10 text-[17px]">{handleCurrentLoc()}</p>
+        <div className="flex items-center h-12 w-full bg-gray-100 text-primary rounded-tr-md rounded-br-md 
+                        sticky top-17 border-l-blue-500 border-l-10 font-bold shadow-gray shadow-md">
+            <p className="ml-10 text-[15px]">{handleCurrentLoc()}</p>
         </div>
     );
 }
--- src/layouts/AdminSidemenu.jsx
+++ src/layouts/AdminSidemenu.jsx
@@ -8,13 +8,50 @@ const AdminSidemenu = () => {
         >
             <div className="flex flex-col gap-1 h-fit w-50 rounded-lg">
                 <div className="flex flex-col items-center w-full">
-                    <p>Welcome,</p>
+                    <p className="text-[13px] text-gray-600 mb-1">Welcome,</p>
                     <p className="font-bold text-[20px]">{user.name}</p>
-                    <p className="w-full text-center text-black font-light text-[20px] mt-3 bg-gray-300 rounded-xl p-1">
+                    <p className="w-full text-center text-blue-700 font-mono text-[20px] mt-3 bg-gray-300 rounded-xl p-1">
                         {user.role.toLocaleUpperCase()}
                     </p>
                 </div>
+                <div className="h-fit w-full mt-12">
+                    <p className="font-bold text-[13px] text-gray-600 mb-3">Menus</p>
+                    <SideMenuBtn />
+                </div>
             </div>
+        </div>
+    );
+}
+
+const SideMenuBtn = () => {
+
+    return (
+        <div className="flex flex-col gap-3 w-full text-white font-light">
+            <button className="flex items-center gap-3 h-11 w-full bg-primary-500 rounded-tr-full
+                               rounded-br-full cursor-pointer hover:bg-primary hover:scale-103 transition-all">
+                <div className="h-full w-2 bg-indigo-800"></div>
+                <p>Dashboard</p>
+            </button>
+            <button className="flex items-center gap-3 h-11 w-full bg-primary-500 cursor-pointer
+                               rounded-tr-full rounded-br-full hover:bg-primary hover:scale-103 transition-all">
+                <div className="h-full w-2 bg-violet-500"></div>
+                <p>Sales</p>
+            </button>
+            <button className="flex items-center gap-3 h-11 w-full bg-primary-500 cursor-pointer
+                               rounded-tr-full rounded-br-full hover:bg-primary hover:scale-103 transition-all">
+                <div className="h-full w-2 bg-pink-500"></div>
+                <p>Inventory</p>
+            </button>
+            <button className="flex items-center gap-3 h-11 w-full bg-primary-500 cursor-pointer
+                               rounded-tr-full rounded-br-full hover:bg-primary hover:scale-103 transition-all">
+                <div className="h-full w-2 bg-rose-500"></div>
+                <p>Transactions</p>
+            </button>
+            <button className="flex items-center gap-3 h-11 w-full bg-primary-500 cursor-pointer
+                               rounded-tr-full rounded-br-full hover:bg-primary hover:scale-103 transition-all">
+                <div className="h-full w-2 bg-slate-600"></div>
+                <p>Users</p>
+            </button>
         </div>
     );
 }
