8a607f3b8f3d8081cf0c09e62b9ecd2604e25c94
rjbelleza <rjbelleza19@gmail.com>
2025-06-23 18:23:10 +0800

    Finalized redeployment

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://aquasprings-pos-api.onrender.com/api',
 });
 
 // Attach Authorization token to every request
